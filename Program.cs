8796c088c85854cd02cc52a91d29b6ee78b9639f
Julian Schrenk <kjs@fastmail.com>
2018-10-10 08:56:38 +0100

    add reminder about required arguments

--- Program.cs
+++ Program.cs
@@ -74,23 +74,34 @@ namespace Kolmogorov_Smirnov
         }
         static void Main(string[] args)
         {
-            var fileA = args[0];
-            var fileB = args[1];
-            var cdfA = new cdf(readData(fileA));
-            var cdfB = new cdf(readData(fileB));
-            Console.WriteLine($"Read data with {cdfA.Count} and {cdfB.Count} distinct points.");
-            Console.WriteLine($"Read data with {cdfA.TotalNumber} and {cdfB.TotalNumber} total points.");
-            Console.WriteLine(fileA);
-            cdfA.computeCdf();
-            foreach (var r in cdfA.result)
+            try
             {
-                Console.WriteLine($"{r.Key, -5}{r.Value, 5}");
+                if (args.Length != 2)
+                {
+                    throw new ArgumentNullException("Two file names expected. Pass them as command line arguments.");
+                }
+                var fileA = args[0];
+                var fileB = args[1];
+                var cdfA = new cdf(readData(fileA));
+                var cdfB = new cdf(readData(fileB));
+                Console.WriteLine($"Read data with {cdfA.Count} and {cdfB.Count} distinct points.");
+                Console.WriteLine($"Read data with {cdfA.TotalNumber} and {cdfB.TotalNumber} total points.");
+                Console.WriteLine(fileA);
+                cdfA.computeCdf();
+                foreach (var r in cdfA.result)
+                {
+                    Console.WriteLine($"{r.Key, -5}{r.Value, 5}");
+                }
+                Console.WriteLine(fileB);
+                cdfB.computeCdf();
+                foreach (var r in cdfB.result)
+                {
+                    Console.WriteLine($"{r.Key, -5}{r.Value, 5}");
+                }
             }
-            Console.WriteLine(fileB);
-            cdfB.computeCdf();
-            foreach (var r in cdfB.result)
+            catch (Exception ex)
             {
-                Console.WriteLine($"{r.Key, -5}{r.Value, 5}");
+                Console.WriteLine(ex);
             }
         }
     }
